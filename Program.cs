a661d80927c801943c852e5e836d055059af04dd
shahbozkhurramov <shahbozkhurramov@gmail.com>
2022-03-29 14:44:50 +0500

    Bubble Sort

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Algorithms.Sorting;
+
+namespace Algorithms;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = {1, 10, 6, 3, 4};
+        BubbleSort.RecursionSort(arr, arr.Length);
+        for (var i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    } 
+}
---
+++ Sorting/InsertionSort.cs
@@ -0,0 +1,13 @@
+namespace Algorithms.Sorting;
+
+public static class InsertionSort
+{
+    public static void Sort(int[] arr)
+    {
+        int length = arr.Length;
+        for (var i = 0; i < length; i++)
+        {
+            
+        }
+    }
+}
