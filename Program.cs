a0312e866b447f377e7822aa2b56f597ff215075
shahbozkhurramov <shahbozkhurramov@gmail.com>
2022-03-29 15:24:26 +0500

    Insertion Sort

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ public class Program
     static void Main(string[] args)
     {
         int[] arr = {1, 10, 6, 3, 4};
-        BubbleSort.RecursionSort(arr, arr.Length);
+        SelectionSort.Sort(arr);
         for (var i = 0; i < arr.Length; i++)
         {
             Console.WriteLine(arr[i]);
--- Sorting/InsertionSort.cs
+++ Sorting/InsertionSort.cs
@@ -5,9 +5,16 @@ public static class InsertionSort
     public static void Sort(int[] arr)
     {
         int length = arr.Length;
-        for (var i = 0; i < length; i++)
+        for (var i = 1; i < length; i++)
         {
-            
+            int key = arr[i];
+            int j = i - 1;
+            while(key < arr[j] && j >= 0)
+            {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key; 
         }
     }
 }
